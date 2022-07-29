0b4dc5445b8415dc589e885f6517dcd4a668d2e0
Flávio Félix <fau_felix30@hotmail.com>
2022-07-29 17:55:37 -0300

    Começando a seção sobre função

---
+++ secao_5_funcao/01_definir_funcoes/scripts.js
@@ -0,0 +1,16 @@
+function imprimirNoConsole(){
+  console.log("Olá mundo!");
+}
+imprimirNoConsole();
+
+function imprimirUmNumero(num){
+  console.log("O numero é " + num);
+}
+imprimirUmNumero(2);
+imprimirUmNumero(4);
+
+const numeroAleatorio = function(){
+  console.log(Math.random());
+}
+
+numeroAleatorio();
